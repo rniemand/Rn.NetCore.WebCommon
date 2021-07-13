84c6604394f19794f8fd1422be9e4edabd3a5957
Richard Niemand <niemand.richard@gmail.com>
2021-07-12 21:36:47 -0600

    Initial Test

---
+++ src/Rn.NetCore.WebCommon.T1.Tests/Root/MetricPlaceholderTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Rn.NetCore.WebCommon.T1.Tests.Root
+{
+  [TestFixture]
+  public class MetricPlaceholderTests
+  {
+    [Test]
+    public void MetricPlaceholder_None_ShouldReturn_ExpectedValue()
+    {
+      Assert.AreEqual("(none)", MetricPlaceholder.None);
+    }
+
+    [Test]
+    public void MetricPlaceholder_Unknown_ShouldReturn_ExpectedValue()
+    {
+      Assert.AreEqual("(unknown)", MetricPlaceholder.Unknown);
+    }
+
+    [Test]
+    public void MetricPlaceholder_Unset_ShouldReturn_ExpectedValue()
+    {
+      Assert.AreEqual("(unset)", MetricPlaceholder.Unset);
+    }
+  }
+}
