a17fdebc5b12e22ca2dae30cf0e7149ed7f13838
Richard Niemand <niemand.richard@gmail.com>
2021-09-15 14:48:50 -0600

    Porting attributes

---
+++ src/Rn.NetCore.WebCommon/Models/Requests/BaseApiRequest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using Rn.NetCore.WebCommon.ModelBinders;
+using Rn.NetCore.WebCommon.Models.Dto;
+
+namespace Rn.NetCore.WebCommon.Models.Requests
+{
+  [ModelBinder(BinderType = typeof(BaseApiRequestModelBinder))]
+  public abstract class BaseApiRequest
+  {
+    [OpenApiIgnore]
+    public UserDto User { get; set; }
+
+    [OpenApiIgnore]
+    public int UserId { get; set; }
+
+    protected BaseApiRequest()
+    {
+      // TODO: [TESTS] (BaseApiRequest) Add tests
+      User = null;
+      UserId = 0;
+    }
+  }
+}
